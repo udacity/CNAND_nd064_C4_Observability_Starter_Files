b60a562c6801b47d01c207b4d4d27348d1671e7d
Jason "Jay" Smith <jay@thejaysmith.com>
2020-12-04 16:21:54 -0800

    Added scripts/k3s.sh for Vagrantfile

---
+++ k3s.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "**** Begin installing k3s"
+
+#Install
+curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=v1.19.2+k3s1 K3S_KUBECONFIG_MODE="644" sh -
+echo "**** End installing k3s"
+
+#kubectl proxy --address='0.0.0.0' /dev/null &
