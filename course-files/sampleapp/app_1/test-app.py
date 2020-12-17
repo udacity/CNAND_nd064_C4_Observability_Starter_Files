ce50da1db36ec789ca93dc843e5c5966367fde2b
Jason "Jay" Smith <jay@thejaysmith.com>
2020-12-17 13:56:35 -0800

    base course files

---
+++ course-files/sampleapp/app_1/test-app.py
@@ -0,0 +1,38 @@
+import os
+import requests
+from flask import Flask, render_template, request, jsonify
+from jaeger_client import Config
+from flask_opentracing import FlaskTracing
+
+app = Flask(__name__)
+config = Config(
+    config={
+        'sampler':
+        {'type': 'const',
+         'param': 1},
+                        'logging': True,
+                        'reporter_batch_size': 1,},
+                        service_name="service")
+jaeger_tracer = config.initialize_tracer()
+tracing = FlaskTracing(jaeger_tracer, True, app)
+
+
+@app.route('/')
+def hello_world():
+
+    return f"""Hello, World!
+
+Thank you for visiting the site!\n\n"""
+
+
+
+@app.route('/api/second', methods=['GET', 'POST'])
+def jobs():
+    if request.method == 'GET':
+        response = requests.get("http://second-sample-app.default.svc.cluster.local:8000")
+        return str(type(response))
+    elif request.method == 'POST':
+        response = requests.get("http://second-sample-app.default.svc.cluster.local:8000")
+        return str(type(response))
+
+
---
+++ course-files/sampleapp/app_2/test-app.py
@@ -0,0 +1,49 @@
+import os
+import requests
+from flask import Flask, render_template, request, jsonify
+
+from jaeger_client import Config
+from jaeger_client.metrics.prometheus import PrometheusMetricsFactory
+from flask_opentracing import FlaskTracing
+from flask_opentracing import FlaskTracer
+from opentracing import tracer
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    homepages = []
+    gh_jobs = 'https://jobs.github.com/positions.json?description=python'
+
+    parent_span = flask_tracer.get_span()
+    #return render_template("main.html")
+    with tracer.start_span('get-python-jobs', child_of=parent_span) as span:
+        span.set_tag("http.url", gh_jobs)
+        res = requests.get(gh_jobs)
+        span.set_tag("http.status_code", res.status_code)
+
+    with tracer.start_span('get-json', child_of=parent_span) as span:
+        myjson = res.json()
+
+        span.set_tag("python_jobs", len(myjson))
+        pull_python_jobs = map(lambda item: item['title'], myjson)
+
+
+
+    return "Tracing Results: " + ", ".join(pull_python_jobs)
+
+
+
+
+def initialize_trace():
+  config = Config(
+      config={
+          'sampler': {'type': 'const', 'param': 1}
+      },
+      service_name='service')
+  return config.initialize_tracer() # also sets opentracing.tracer
+
+
+
+flask_tracer = FlaskTracer(initialize_trace, True, app)
+https://github.com/jaegertracing/jaeger/tree/master/monitoring/jaeger-mixin
