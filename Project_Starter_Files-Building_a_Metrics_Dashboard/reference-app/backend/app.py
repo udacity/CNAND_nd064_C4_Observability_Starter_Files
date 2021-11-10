7b765ad8f3d3becaa2c7bb82d3240c1770861f6e
Mpho Mphego <mpho112@gmail.com>
2021-11-10 08:27:55 +0200

    Fix various errors on flask application

--- Project_Starter_Files-Building_a_Metrics_Dashboard/reference-app/backend/app.py
+++ Project_Starter_Files-Building_a_Metrics_Dashboard/reference-app/backend/app.py
@@ -5,30 +5,35 @@ from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
-app.config['MONGO_DBNAME'] = 'example-mongodb'
-app.config['MONGO_URI'] = 'mongodb://example-mongodb-svc.default.svc.cluster.local:27017/example-mongodb'
+app.config["MONGO_DBNAME"] = "example-mongodb"
+app.config[
+    "MONGO_URI"
+] = "mongodb://example-mongodb-svc.default.svc.cluster.local:27017/example-mongodb"
 
 mongo = PyMongo(app)
 
-@app.route('/')
+
+@app.route("/")
 def homepage():
     return "Hello World"
 
 
-@app.route('/api')
+@app.route("/api")
 def my_api():
     answer = "something"
     return jsonify(repsonse=answer)
 
-@app.route('/star', methods=['POST'])
+
+@app.route("/star", methods=["POST"])
 def add_star():
-  star = mongo.db.stars
-  name = request.json['name']
-  distance = request.json['distance']
-  star_id = star.insert({'name': name, 'distance': distance})
-  new_star = star.find_one({'_id': star_id })
-  output = {'name' : new_star['name'], 'distance' : new_star['distance']}
-  return jsonify({'result' : output})
+    star = mongo.db.stars
+    name = request.json["name"]
+    distance = request.json["distance"]
+    star_id = star.insert({"name": name, "distance": distance})
+    new_star = star.find_one({"_id": star_id})
+    output = {"name": new_star["name"], "distance": new_star["distance"]}
+    return jsonify({"result": output})
+
 
 if __name__ == "__main__":
     app.run()
